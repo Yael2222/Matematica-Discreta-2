159c985fd87477ef690b3c0e44fd0efc3f1cba41
Fernando Yael Herrera Tamayo <00038623@uca.edu.sv>
2023-11-28 18:15:47 -0600

    Update dados.cpp

--- dados.cpp
+++ dados.cpp
@@ -1,39 +1,52 @@
 #include <iostream>
-#include <cstdlib>  // Para la función rand()
-#include <ctime>    // Para la función time()
-
-using namespace std;
+#include <random>
+#include <ctime>
 
 int main() {
-    // Inicializar la semilla para obtener números aleatorios diferentes en cada ejecución
-    srand(time(0));
+    // Número de simulaciones a realizar
+    int num_simulations = 100000000;
 
-    const float numLanzamientos = 10000; // Puedes ajustar este número según tus necesidades
-   float conteoExitos = 0;
+    // Contadores para los eventos A, B y A ∩ B
+    int count_A = 0, count_B = 0, count_A_and_B = 0;
 
-    for (int i = 0; i < numLanzamientos; ++i) {
-        // Lanzar dos dados (valores entre 1 y 6)
-        int dado1 = rand() % 6 + 1;
-        int dado2 = rand() % 6 + 1;
+    // Inicializa el generador de números aleatorios
+    std::mt19937 rng(time(0));
+    std::uniform_int_distribution<int> die(1, 6);
 
-        // Verificar la condición: la suma de puntos es ≥ 9 y la cara 6 aparece en al menos uno de los dados
-        if (( dado1 == 6 || dado2 == 6)) {
-            if (dado1 + dado2 >= 9)
-            {
-                conteoExitos++;
+    // Realiza las simulaciones
+    for (int i = 0; i < num_simulations; ++i) {
+        // Lanza dos dados
+        int die1 = die(rng);
+        int die2 = die(rng);
+
+        // Comprueba si se cumple el evento A (la suma de los puntos es mayor o igual a 9)
+        if (die1 + die2 >= 9) {
+            ++count_A;
+        }
+
+        // Comprueba si se cumple el evento B (al menos un dado muestra un 6)
+        if (die1 == 6 || die2 == 6) {
+            ++count_B;
+
+            // Si se cumple el evento B, comprueba si también se cumple el evento A
+            if (die1 + die2 >= 9) {
+                ++count_A_and_B;
             }
-            
         }
     }
 
-    // Calcular la probabilidad
-   float probabilidad = (conteoExitos) / numLanzamientos;
+    // Calcula las probabilidades
+    double P_A = static_cast<double>(count_A) / num_simulations;
+    double P_B = static_cast<double>(count_B) / num_simulations;
+    double P_A_and_B = static_cast<double>(count_A_and_B) / num_simulations;
 
-    // Mostrar resultados
-    cout << "Número de lanzamientos: " << numLanzamientos << endl;
-    cout << "Número de casos favorables: " << conteoExitos << endl;
-    cout << "Probabilidad P(la suma de puntos sea >= 9 | la cara 6 aparece): " << probabilidad << endl;
+    // Calcula la probabilidad condicional P(A|B)
+    double P_A_given_B = P_A_and_B / P_B;
+
+    // Imprime el resultado
+    std::cout << "P(A|B) = " << P_A_given_B << std::endl;
 
     return 0;
 }
+
 
